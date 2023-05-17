9bf1cb9c878c877e35ca077c69b823ecb0675338
SaugatMgr <111353294+SaugatMgr@users.noreply.github.com>
2023-05-17 15:44:33 +0545

    Update Merolagani_Scraping.py
    removed unnecessary lines

--- Merolagani_Scraping.py
+++ Merolagani_Scraping.py
@@ -57,9 +57,3 @@ try:
             csv_writer.writerow(each_row)
 except Exception as e:
     print(e)
-
-my_headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS \
-                            X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) \
-                            Chrome/71.0.3578.98 Safari/537.36",
-              "Accept": "text/html,application/xhtml+xml,application/xml; \
-                        q=0.9,image/webp,image/apng,*/*;q=0.8"}
